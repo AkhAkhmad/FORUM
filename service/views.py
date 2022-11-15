e5d9a80661b1206988c1251338cae6436507b083
AkhAkhmad <Am9797920@icloud.com>
2022-11-15 22:56:27 +0300

    Added func create_post and deleted her class

--- service/views.py
+++ service/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render
-from service.models import Post, Comment
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from .forms import UserRegisterForm
-# from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from service.models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
 
 
-class RegisterForm(UserPassesTestMixin, CreateView):
+class RegisterForm(SuccessMessageMixin, CreateView):
     form_class = UserRegisterForm
+    success_message = '%(username)s was created successfully'
     template_name = 'register.html'
     success_url = reverse_lazy('login')
-
-    def test_func(self):
-        return self.request.user.email.endswith('.com')
 
 
 # @login_required
@@ -38,12 +39,25 @@ class DetailPostView(DetailView):
     template_name = 'detail.post.html'
 
 
-class CreatePostView(PermissionRequiredMixin, CreateView):  # PermissionRequiredMixin включает в себя LoginRequiredMixin
-    permission_required = 'service.added_post'  # вывод ошибки, если нет прав
-    model = Post
-    template_name = 'create_post.html'
-    # form_class = PostForm
-    fields = '__all__'
+# class CreatePostView(PermissionRequiredMixin, CreateView):  # PermissionRequiredMixin включает в себя LoginRequiredMixin
+#     permission_required = 'service.added_post'  # вывод ошибки, если нет прав
+#     model = Post
+#     template_name = 'create_post.html'
+#     form_class = PostForm
+# fields = '__all__'
+
+@login_required
+@permission_required('service.add_post')
+def create_post(req):
+    form = PostForm()
+    if req.method == 'POST':
+        form = PostForm(req.POST)
+        if form.is_valid():
+            form.save()
+            title = form.cleaned_data.get('title')
+            messages.success(req, f'Post {title} was creates successfully')
+            return redirect('index')
+    return render(req, 'create_post.html', {'form': form})
 
 
 class UpdatePostView(PermissionRequiredMixin, UpdateView):
@@ -63,8 +77,8 @@ class DeletePostView(PermissionRequiredMixin, DeleteView):
 class AddCommentView(LoginRequiredMixin, CreateView):
     model = Comment
     template_name = 'add_comment.html'
-    fields = '__all__'
+    form_class = CommentForm
 
     # def form_valid(self, form):
-    #     form.instanсe.post_id = self.kwargs('pk') # присваевает ключ из передаваемого параметра
+    #     form.instanсe.post_id = self.kwargs('pk')  # присваевает ключ из передаваемого параметра
     #     return super().form_valid(form)
