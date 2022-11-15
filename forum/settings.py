1304b2c4612accb3d09a18f9ba0ee305dad4f397
AkhAkhmad <Am9797920@icloud.com>
2022-11-15 17:44:24 +0300

    added login forms

--- forum/settings.py
+++ forum/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'service',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -92,5 +93,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'root/static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- service/views.py
+++ service/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render, redirect
 from service.models import Post, Comment
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from .forms import UserRegisterForm
+
+
+# from django.contrib.auth.forms import UserCreationForm
+
+
+class RegisterForm(CreateView):
+    form_class = UserRegisterForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
 
 
 def index(req):
