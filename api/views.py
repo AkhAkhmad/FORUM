9f4e0260d81653c44ba315733e517126b979c8a4
AkhAkhmad <Am9797920@icloud.com>
2022-11-16 20:53:32 +0300

    api on class(APIView)

--- api/views.py
+++ api/views.py
@@ -1,54 +1,40 @@
-from rest_framework import viewsets
 from api.models import Checkbox
 from api.serializers import CheckboxSerializer
-from rest_framework.decorators import api_view
+from rest_framework import status, permissions
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.views import APIView
 
 
-# class CheckboxViewSet(viewsets.ModelViewSet):
-#     queryset = Checkbox.objects.all()
-#     serializer_class = CheckboxSerializer
+class CheckboxList(APIView):
+    # authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAdminUser]
 
-@api_view(['GET'])
-def checkbox_list(req):
-    checkboxes = Checkbox.objects.all()
-    serializer = CheckboxSerializer(checkboxes, many=True)
-    return Response(serializer.data)
+    def get(self, reg, format=None):
+        checkboxes = Checkbox.objects.all()
+        serializer = CheckboxSerializer(checkboxes, many=True)
+        return Response(serializer.data)
 
-
-@api_view(['GET'])
-def checkbox_detail(req, pk):
-    try:
-        checkbox = Checkbox.objects.get(pk=pk)
-        serializer = CheckboxSerializer(checkbox)
-    except Checkbox.DoesNotExist:
-        return Response({'error': f'Checkbox with id = {pk} is not found'}, status=status.HTTP_404_NOT_FOUND)
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def checkbox_create(req):
-    serializer = CheckboxSerializer(data=req.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data, status.HTTP_201_CREATED)
-
-
-@api_view(['PUT'])
-def checkbox_update(req, pk):
-    try:
-        checkbox = Checkbox.objects.get(pk=pk)
-        serializer = CheckboxSerializer(data=req.data)  # передаем данные
+    def post(self, reg, format=None):
+        serializer = CheckboxSerializer(data=reg.data)
         if serializer.is_valid():
             serializer.save()
-    except Checkbox.DoesNotExist:
-        return Response({'error': f'Checkbox with id = {pk} is not found'}, status=status.HTTP_404_NOT_FOUND)
-    return Response(serializer.data)
+        return Response(serializer.data, status.HTTP_201_CREATED)
 
 
-@api_view(['DELETE'])
-def checkbox_delete(req, pk):
-    checkbox = Checkbox.objects.get(pk=pk)
-    checkbox.delete()
-    return Response(status == status.HTTP_204_NO_CONTENT)
+class CheckboxDetail(APIView):
+    permission_classes = [permissions.IsAdminUser]
+
+    def put(self, reg, pk, format=None):
+        try:
+            checkbox = Checkbox.objects.get(pk=pk)
+            serializer = CheckboxSerializer(data=reg.data)  # передаем данные
+            if serializer.is_valid():
+                serializer.save()
+        except Checkbox.DoesNotExist:
+            return Response({'error': f'Checkbox with id = {pk} is not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.data)
+
+    def delete(self, reg, pk, format=None):
+        checkbox = Checkbox.objects.get(pk=pk)
+        checkbox.delete()
+        return Response(status == status.HTTP_204_NO_CONTENT)
