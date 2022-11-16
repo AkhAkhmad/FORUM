a09acd2efc824e19e4e54fa3c29d32e7e83e4390
AkhAkhmad <Am9797920@icloud.com>
2022-11-17 01:30:03 +0300

    fix

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from api import views
 
 urlpatterns = [
     path('checkbox_list/', views.CheckboxList.as_view(), name='checkbox_list'),
+    path('checkbox_detail/<int:pk>', views.CheckboxDetail.as_view(), name='checkbox_detail'),
     path('data', views.DataView.as_view()),
 ]
